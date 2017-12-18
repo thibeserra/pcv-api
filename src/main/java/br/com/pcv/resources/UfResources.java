60c0a224e169b3970ba3e45593631cfeb5e4416a
= <=>
2017-12-18 20:57:36 -0200

    Criação de Serviço Restfull que retorna estado Brasileiro de acordo com o ID informado

--- src/main/java/br/com/pcv/resources/UfResources.java
+++ src/main/java/br/com/pcv/resources/UfResources.java
@@ -3,6 +3,7 @@ package br.com.pcv.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +12,21 @@ import br.com.pcv.domain.Uf;
 import br.com.pcv.repository.UfsRepository;
 
 @RestController
+@RequestMapping("/ufs")
 public class UfResources {
 	
 	@Autowired
 	private UfsRepository ufsRepository;
 	
-	@RequestMapping(value="/ufs", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Uf> listar() {
 		
 		return this.ufsRepository.findAll();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Uf buscaUf(@PathVariable Long id) {
+		return this.ufsRepository.findOne(id);
 	}
 	
 }
